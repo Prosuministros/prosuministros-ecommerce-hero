77df0b1313021c5ab9c1ac3980839ae2b8ff3085
EmmaMCastillo <emma.castillo@tdxcore.com>
2025-07-11 09:22:12 -0600

    Se modifico la imagen del equipo

--- src/components/EquipoInfo.tsx
+++ src/components/EquipoInfo.tsx
@@ -4,7 +4,7 @@ const EquipoInfo = () => {
   return (
     <section className="w-full bg-white">
       <img
-        src="/img/BannerProsuministros06.jpg"
+        src="/img/BannerProsuministros06.png"
         alt="Equipo ProSuministros"
         className="w-full h-auto object-cover"
         style={{ display: 'block' }}
