33bbb7044a84c361e53b9d3709b260e47c51bffa
Prosuministros <gerencia@prosuministros.com>
2025-07-16 14:55:17 -0600

    Update Contacto.tsx

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -316,7 +316,7 @@ const Contacto = () => {
             <div>
               <h4 className="font-semibold mb-4">Redes Sociales</h4>
               <div className="flex justify-center mt-2">
-                <a href="https://www.linkedin.com/company/prosuministros/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" className="text-[#00C8CF] hover:text-white text-3xl">
+                <a href="https://www.linkedin.com/company/prosuministros-de-colombia/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn" className="text-[#00C8CF] hover:text-white text-3xl">
                   {/* Icono de LinkedIn */}
                   <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" width="1em" height="1em"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm15.5 11.268h-3v-5.604c0-1.337-.025-3.063-1.868-3.063-1.868 0-2.154 1.459-2.154 2.968v5.699h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.838-1.563 3.036 0 3.597 2.001 3.597 4.601v5.595z"/></svg>
                 </a>
